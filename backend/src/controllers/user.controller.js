c907114d847fc9bdcaedf26b01f506c06dbce92b
tuan.vuthanh <tuan.vuthanh@ncc.asia>
2021-10-25 09:36:49 +0700

    remove paginate plugin

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,4 @@
 const httpStatus = require('http-status');
-const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { userService } = require('../services');
@@ -7,13 +6,6 @@ const { userService } = require('../services');
 const createUser = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
   res.status(httpStatus.CREATED).send(user);
-});
-
-const getUsers = catchAsync(async (req, res) => {
-  const filter = pick(req.query, ['name', 'role']);
-  const options = pick(req.query, ['sortBy', 'limit', 'page']);
-  const result = await userService.queryUsers(filter, options);
-  res.send(result);
 });
 
 const getUser = catchAsync(async (req, res) => {
@@ -36,7 +28,6 @@ const deleteUser = catchAsync(async (req, res) => {
 
 module.exports = {
   createUser,
-  getUsers,
   getUser,
   updateUser,
   deleteUser,
--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -16,20 +16,6 @@ const createUser = async (userBody) => {
 };
 
 /**
- * Query for users
- * @param {Object} filter - Mongo filter
- * @param {Object} options - Query options
- * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
- * @param {number} [options.limit] - Maximum number of results per page (default = 10)
- * @param {number} [options.page] - Current page (default = 1)
- * @returns {Promise<QueryResult>}
- */
-const queryUsers = async (filter, options) => {
-  const users = await User.paginate(filter, options);
-  return users;
-};
-
-/**
  * Get user by id
  * @param {ObjectId} id
  * @returns {Promise<User>}
@@ -82,7 +68,6 @@ const deleteUserById = async (userId) => {
 
 module.exports = {
   createUser,
-  queryUsers,
   getUserById,
   getUserByEmail,
   updateUserById,
